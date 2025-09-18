f1294de60264a842feb08b5fde7d111f4813481e
Kumar-Prasannajit <kumarprasannajitsahu@gmail.com>
2025-09-18 12:57:22 +0530

    set base in the vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // <-- important: makes asset paths relative
   build: {
     rollupOptions: {
       // Treat Font Awesome React as external (prevents Rollup from bundling it incorrectly)
